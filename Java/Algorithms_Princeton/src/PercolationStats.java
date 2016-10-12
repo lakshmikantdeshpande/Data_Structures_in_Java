ce0bab7f82eba5e0b1509a874389030c72eb838e
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2016-10-12 14:45:21 +0530

    Submitted assignment 1 (93 / 100)
    Failed first time
    Passed second time (Care should be taken to make fields private)
    Gotta fix findbugs issues to get a perfect 100/100 score

---
+++ Java/Algorithms_Princeton/src/PercolationStats.java
@@ -0,0 +1,54 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+	private double[] trials;
+
+	public PercolationStats(int n, int tests) {
+
+		if (n <= 0 || tests <= 0)
+			throw new IllegalArgumentException();
+
+		trials = new double[tests];
+
+		for (int i = 0; i < tests; i++) {
+
+			Percolation perc = new Percolation(n);
+			int steps = 0;
+			while (!perc.percolates()) {
+				int row = StdRandom.uniform(n) + 1;
+				int column = StdRandom.uniform(n) + 1;
+				if (!perc.isOpen(row, column)) {
+					perc.open(row, column);
+					steps++;
+				}
+			}
+
+			trials[i] = (double) steps / (n * n);
+		}
+	}
+
+	public double mean() {
+		return StdStats.mean(trials);
+	}
+
+	public double stddev() {
+		return StdStats.stddev(trials);
+	}
+
+	public double confidenceLo() {
+		return mean() - ((1.96 * stddev()) / Math.sqrt(trials.length));
+	}
+
+	public double confidenceHi() {
+		return mean() + ((1.96 * stddev()) / Math.sqrt(trials.length));
+	}
+
+	public static void main(String[] args) {
+		PercolationStats ps = new PercolationStats(300, 1000);
+		StdOut.print("Mean = " + ps.mean() + "\n");
+		StdOut.print("Standard Deviation = " + ps.stddev() + "\n");
+		StdOut.print("95% Confidence Interval = " + ps.confidenceLo() + ", " + ps.confidenceHi());
+	}
+}
