2f42ed95fd1b02b8122d37cfaa5ae3b46b84b30a
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2016-10-18 11:52:16 +0530

    Assignment 2 : Deque Implemented
    RandomizedQueue remaining

--- Java/Algorithms_Princeton/src/Deque.java
+++ Java/Algorithms_Princeton/src/Deque.java
@@ -1,12 +1,13 @@
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
-    private Node head = null, tail = null;
-    private int size = 0;
+    private Node head, tail;
+    private int size;
 
     public Deque() {
-        head = new Node();
-        tail = head;
+    	head = tail = null;
+    	size = 0;
     }
 
     private class Node{
@@ -16,7 +17,7 @@ public class Deque<Item> implements Iterable<Item> {
     }
 
     public boolean isEmpty(){
-        return head == null;
+        return this.size == 0;
     }
 
     public int size()
@@ -25,23 +26,82 @@ public class Deque<Item> implements Iterable<Item> {
     }
 
     public void addFirst(Item item){
-        Node oldtail = tail;
-        tail = new Node();
-        tail.item = item;
-
-        if (isEmpty())
-            head = tail;
-        else // tail exists
-        {
-            tail.next = oldtail;
-            if(oldtail.prev
-        }
-
-        size++;
+    	if (item == null)
+    		throw new NullPointerException();
+    	
+    	if (this.isEmpty())
+    	{
+    		head = new Node();
+    		head.item = item;
+    		head.next = null;
+    		head.prev = null;
+    		tail = head;
+    	}
+    	else
+    	{
+    		Node oldhead = head;
+    		head = new Node();
+    		head.item = item;
+    		head.next = oldhead;
+    		oldhead.prev = head;
+    		head.prev = null;
+    	}
+    	size++;
     }
 
-    public void addLast(Item item){}
-
+    public void addLast(Item item){
+    	if (item == null)
+    		throw new NullPointerException();
+    	
+    	if (this.isEmpty())
+    	{
+    		tail = new Node();
+    		tail.item = item;
+    		tail.next = null;
+    		tail.prev = null;
+    		head = tail;
+    	}
+    	else
+    	{
+    		Node oldtail = tail;
+    		tail= new Node();
+    		tail.item = item;
+    		tail.next = null;
+    		tail.prev = oldtail;
+    		oldtail.next = tail;
+    	}
+    	size++;
+    }
+    
+    public Item removeFirst()
+    {
+    	if (this.size == 0)
+    		throw new NoSuchElementException();
+    	this.size--;
+    	Item item = head.item;
+    	head = head.next;
+    	if (isEmpty())
+    		tail = null;
+    	else
+    		head.prev = null;
+    	return item;
+    }
+    
+    public Item removeLast()
+    {
+    	if (this.size == 0)
+    		throw new NoSuchElementException();
+    	this.size--;
+    	Item item = tail.item;
+    	tail = tail.prev;
+    	if (isEmpty())
+    		head = null;
+    	else
+    		tail.next = null;
+    	
+    	return item;
+    }
+    
     public DequeComparator iterator()
     {
         return new DequeComparator();
@@ -62,8 +122,27 @@ public class Deque<Item> implements Iterable<Item> {
             // Not Implemented
         }
     }
-
-    public static void main(String args[]){
-
+    
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void main(String args[]){
+		Deque deq = new Deque();
+    	deq.addFirst("hi");
+    	deq.addLast(0);
+    	deq.addLast(2);
+    	deq.addFirst(1);
+    	deq.addFirst("zuzu");
+    	
+//    	deq.removeFirst();
+//    	deq.removeFirst();
+//    	deq.removeFirst();
+//    	deq.removeLast();
+//    	deq.removeFirst ();
+//    	
+    	Iterator iterator = deq.iterator();
+    	while (iterator.hasNext())
+    	{
+    		System.out.println(iterator.next());
+    	}
+    	
     }
 }
