bc2df07f6cd98918fb40721a99531c07d6d8d692
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2016-11-07 14:09:03 +0530

    Basic BST (symbol table (hashmap))
    BST get and put methods implementation

---
+++ Java/Algorithms_Princeton/src/week_4_PriorityQ_and_BST_Symbol_Tables/PartOne/BST_basic.java
@@ -0,0 +1,66 @@
+package week_4_PriorityQ_and_BST_Symbol_Tables.PartOne;
+
+public class BST_basic {
+	private static Node root;
+	
+	private static class Node
+	{
+		int key;
+		int value;
+		Node left, right;
+		
+		public Node(int key, int value)
+		{
+			this.key = key;
+			this.value = value;
+			this.left = null;
+			this.right = null;
+		}
+	}
+	
+	public static void put(int key, int value)
+	{
+		root = put(root, key, value);
+	}
+	
+	private static Node put(Node root, int key, int value)
+	{
+		if (root == null) 
+			return new Node(key, value);
+		
+		if (key < root.key )
+			root.left = put(root.left, key, value);
+		else if (key > root.key)
+			root.right = put(root.right, key, value);
+		else if (key == root.key)
+			root.value = value;
+		return root;
+	}
+	
+	public static int get(int key)
+	{
+		Node temp = root;
+		while (temp != null)
+		{
+			if (key == temp.key)
+				return temp.value;
+			else if (key > temp.key)
+				temp = temp.right;
+			else if (key < temp.key)
+				temp = temp.left;
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) 
+	{
+		System.out.println(get(5));
+		put(1, 4);
+		put(1, 6);
+		put(2, 5);
+		put(4, 7);
+		put(9, 4);
+		System.out.println(get(9));
+	}
+
+}
