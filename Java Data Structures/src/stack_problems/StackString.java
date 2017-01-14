526fdcaa8219850e1d66201875e307ceb905711e
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-01-14 22:08:57 +0530

    StackString optimized
    Optimized Stack String

--- Java Data Structures/src/stack_problems/StackString.java
+++ Java Data Structures/src/stack_problems/StackString.java
@@ -1,53 +1,62 @@
 // 18. Create an empty stack of integers.
 // 'S' should indicate push and 'X' should indicate a pop. 
 // Is it possible to create 325641 & 154623 by just pushing and popping 1, 2, 3, 4, 5, 6 in this order only?
-// n*n time  n space
+// n*n time (better than n*n that though)  n space 
+
+package stack_problems;
 
 import java.util.Stack;
 
 public class StackString {
+	public static final int MAX = 6;
 
-public static String StringFromStacks(String num) {
-    if (num == null || num.equals("") || num.equals(" "))
-        return "NOT POSSIBLE";
-        
-    StringBuilder sb = new StringBuilder();
-    int output = 0;
-    
-    Stack<Integer> s1 = new Stack<Integer>();
-    Stack<Integer> s2 = new Stack<Integer>();
-    
-    for (int i = 6; i > 0; i--)
-        s2.push(i);
-    
-    int i = 0, x = 0, currchar = 0, t = 0;
-    int number = Integer.parseInt(num);
-    
-    while (x != number) {
-        currchar = Character.getNumericValue(num.charAt(i++));
-        x = x * 10 + currchar;
-        
-        if (!s1.empty() && s1.peek() == currchar) {
-            s1.pop();
-            sb.append("X");
-        } else if (!s2.empty() && s2.contains(currchar)) {
-            while ((t = s2.pop()) != currchar) {
-                s1.push(t);
-                sb.append("S");
-            }
-            s1.push(t);
-            sb.append("S");
-            s1.pop();
-            sb.append("X");                    
-        } else return "NOT POSSIBLE";
-   }
-   return sb.toString();
-}
+	public static String StringFromStacks(String num) {
+		if (num == null || num.equals("") || num.equals(" "))
+			return "NOT POSSIBLE";
 
-public static void main(String args[]) {
-    System.out.println(StringFromStacks("325641"));
-    System.out.println(StringFromStacks("154623"));
-    System.out.println(StringFromStacks("654321"));
-}
+		final StringBuilder sb = new StringBuilder();
+
+		final Stack<Integer> s1 = new Stack<Integer>();
+		final Stack<Integer> s2 = new Stack<Integer>();
+
+		initialize(s2);
+
+		int i = 0, x = 0, currchar = 0;
+		int number = Integer.parseInt(num);
+
+		while (x != number) {
+			currchar = Character.getNumericValue(num.charAt(i++));
+			x = x * 10 + currchar;
+
+			if (!s1.empty() && s1.peek() == currchar) {
+				s1.pop();
+				sb.append("X");
+			} else if (!s2.empty() && s2.peek() <= currchar) {
+				// keep popping until desired number is not found
+				while (!s2.empty() && s2.peek() <= currchar) {
+					s1.push(s2.pop());
+					sb.append("S");
+				}
+
+				s1.pop();
+				sb.append("X");
+			} else
+				return "NOT POSSIBLE";
+		}
+		return sb.toString();
+	}
+
+	// assumes that numbers to be used are sorted
+	// i.e. here 1, 2, 3, 4, 5, 6
+	public static void initialize(Stack<Integer> stack) {
+		for (int i = MAX; i > 0; i--)
+			stack.push(i);
+	}
+
+	public static void main(String args[]) {
+		System.out.println(StringFromStacks("325641"));
+		System.out.println(StringFromStacks("154623"));
+		System.out.println(StringFromStacks("654321"));
+	}
 
 }
--- Java Data Structures/src/stack_problems/ThreeStacksInArray.java
+++ Java Data Structures/src/stack_problems/ThreeStacksInArray.java
@@ -1,151 +1,147 @@
 // 15. Implement three stacks in an array
 // n time n size
 
+package stack_problems;
+
 public class ThreeStacksInArray {
-private int[] array;
-private int size, topOne, topTwo, topThree, baseThree;
+	private int[] array;
+	private int size, topOne, topTwo, topThree, baseThree;
 
-public ThreeStacksInArray(int size) throws Exception {
-    if (size < 3)
-        throw new Exception("Size should be at least 3");
-        
-    this.size = size;
-    array = new int[size];
-    java.util.Arrays.fill(array, Integer.MIN_VALUE);
-    topOne = -1;
-    topTwo = size;
-    baseThree = size/2;
-    topThree = baseThree;
-}
+	public ThreeStacksInArray(int size) throws Exception {
+		if (size < 3)
+			throw new Exception("Size should be at least 3");
 
-public ThreeStacksInArray() throws Exception {
-    this(10);
-}
+		this.size = size;
+		array = new int[size];
+		java.util.Arrays.fill(array, Integer.MIN_VALUE);
+		topOne = -1;
+		topTwo = size;
+		baseThree = size / 2;
+		topThree = baseThree;
+	}
 
-/*
- ------------------------------------------------------------
- |                            |                             |
- |                            |                             |
- |--->                        |--->                     <---|
- ------------------------------------------------------------ 
- topOne              baseThree & topThree              topTwo
- 
-*/
+	public ThreeStacksInArray() throws Exception {
+		this(10);
+	}
 
-public void push(int stackID, int data) throws Exception {
-    if (stackID ==  1) {
-        if (topOne + 1 == baseThree) {
-            if (isMiddleStackRightShiftable()) {
-                shiftMiddleStackToRight();
-                array[++topOne] = data;
-            } else
-                throw new Exception("Stack 1 is full!");
-        } else
-            array[++topOne] = data;
-     } else if (stackID == 2) {
-            if (topTwo - 1 == topThree) {
-                if (isMiddleStackLeftShiftable()) {
-                    shiftMiddleStackToLeft();
-                    array[--topTwo] = data;
-                }
-                else
-                    throw new Exception("Stack 2 is full!");
-            } else
-                array[--topTwo] = data;       
-     } else if (stackID == 3) {
-            if (topTwo - 1 == topThree) {
-                if (isMiddleStackLeftShiftable()) {
-                    shiftMiddleStackToLeft();
-                    array[++topThree] = data;
-                } else 
-                    throw new Exception("Stack 3 is full!");
-            } else array[++topThree] = data;                
-      } else
-         return;
-}
+	/*
+	 * |---> |---> <---| topOne baseThree & topThree topTwo
+	 */
 
-public int pop(int stackID) throws Exception {
-    if (stackID == 1) {
-        if (topOne == -1)
-            throw new Exception("Stack 1 is empty!");
-        int value = array[topOne];
-        array[topOne--] = Integer.MIN_VALUE;
-        return value;
-    } else if (stackID == 2) {
-        if (topTwo == this.size)
-            throw new Exception("Stack 2 is empty!");
-        int value = array[topTwo];
-        array[topTwo++] = Integer.MIN_VALUE;
-        return value;
-    } else if (stackID == 3) {
-        if (topThree == baseThree) 
-            throw new Exception("Stack 3 is empty!");
-        int value = array[topThree];
-        array[topThree--] = Integer.MIN_VALUE;
-        return value;
-    } else
-        return Integer.MIN_VALUE;
-}
+	public void push(int stackID, int data) throws Exception {
+		if (stackID == 1) {
+			if (topOne + 1 == baseThree) {
+				if (isMiddleStackRightShiftable()) {
+					shiftMiddleStackToRight();
+					array[++topOne] = data;
+				} else
+					throw new Exception("Stack 1 is full!");
+			} else
+				array[++topOne] = data;
+		} else if (stackID == 2) {
+			if (topTwo - 1 == topThree) {
+				if (isMiddleStackLeftShiftable()) {
+					shiftMiddleStackToLeft();
+					array[--topTwo] = data;
+				} else
+					throw new Exception("Stack 2 is full!");
+			} else
+				array[--topTwo] = data;
+		} else if (stackID == 3) {
+			if (topTwo - 1 == topThree) {
+				if (isMiddleStackLeftShiftable()) {
+					shiftMiddleStackToLeft();
+					array[++topThree] = data;
+				} else
+					throw new Exception("Stack 3 is full!");
+			} else
+				array[++topThree] = data;
+		} else
+			return;
+	}
 
-public int peek(int stackID) throws Exception {
-    if (stackID == 1) {
-        if (topOne == -1)
-            throw new Exception("Stack 1 is empty!");
-        return array[topOne];
-    } else if (stackID == 2) {
-        if (topTwo == this.size) 
-            throw new Exception("Stack 2 is empty!");
-        return array[topTwo];
-    } else if (stackID == 3) {
-        if (topThree == baseThree)
-            throw new Exception("Stack 3 is empty!");
-        return array[topThree];
-    } else
-        return Integer.MIN_VALUE;
-}
+	public int pop(int stackID) throws Exception {
+		if (stackID == 1) {
+			if (topOne == -1)
+				throw new Exception("Stack 1 is empty!");
+			int value = array[topOne];
+			array[topOne--] = Integer.MIN_VALUE;
+			return value;
+		} else if (stackID == 2) {
+			if (topTwo == this.size)
+				throw new Exception("Stack 2 is empty!");
+			int value = array[topTwo];
+			array[topTwo++] = Integer.MIN_VALUE;
+			return value;
+		} else if (stackID == 3) {
+			if (topThree == baseThree)
+				throw new Exception("Stack 3 is empty!");
+			int value = array[topThree];
+			array[topThree--] = Integer.MIN_VALUE;
+			return value;
+		} else
+			return Integer.MIN_VALUE;
+	}
 
-public boolean isEmpty(int stackID) {
-    switch(stackID) {
-        case 1:
-            return topOne == -1;
-        case 2:
-            return topTwo == this.size;
-        case 3:
-            return (topThree == baseThree);
-        default:
-            return true;
-    }
-}
+	public int peek(int stackID) throws Exception {
+		if (stackID == 1) {
+			if (topOne == -1)
+				throw new Exception("Stack 1 is empty!");
+			return array[topOne];
+		} else if (stackID == 2) {
+			if (topTwo == this.size)
+				throw new Exception("Stack 2 is empty!");
+			return array[topTwo];
+		} else if (stackID == 3) {
+			if (topThree == baseThree)
+				throw new Exception("Stack 3 is empty!");
+			return array[topThree];
+		} else
+			return Integer.MIN_VALUE;
+	}
 
-public void shiftMiddleStackToRight() {
-    for (int i = topThree; i >= baseThree + 1; i--)
-        array[i+1] = array[i];
-    array[baseThree++] = Integer.MIN_VALUE;
-    topThree++;
-}
+	public boolean isEmpty(int stackID) {
+		switch (stackID) {
+		case 1:
+			return topOne == -1;
+		case 2:
+			return topTwo == this.size;
+		case 3:
+			return (topThree == baseThree);
+		default:
+			return true;
+		}
+	}
 
-public void shiftMiddleStackToLeft() {
-    for (int i = baseThree + 1; i <= topThree; i++)
-        array[i] = array[i+1];
-    array[topThree--] = Integer.MIN_VALUE;
-    baseThree--;
-}
+	public void shiftMiddleStackToRight() {
+		for (int i = topThree; i >= baseThree + 1; i--)
+			array[i + 1] = array[i];
+		array[baseThree++] = Integer.MIN_VALUE;
+		topThree++;
+	}
 
-private boolean isMiddleStackLeftShiftable() {
-    return topOne + 1 < baseThree;
-}
+	public void shiftMiddleStackToLeft() {
+		for (int i = baseThree + 1; i <= topThree; i++)
+			array[i] = array[i + 1];
+		array[topThree--] = Integer.MIN_VALUE;
+		baseThree--;
+	}
 
-private boolean isMiddleStackRightShiftable() {
-    return topThree + 1 < topTwo;
-}
+	private boolean isMiddleStackLeftShiftable() {
+		return topOne + 1 < baseThree;
+	}
 
-public static void main(String args[]) throws Exception {
-    ThreeStacksInArray tsia = new ThreeStacksInArray();
-    tsia.push(3, 1);
-    tsia.push(3, 1);
-    tsia.push(3, 1);
-    tsia.push(3, 1);
-    tsia.push(3, 1);
-    tsia.push(3, 1);
-}
+	private boolean isMiddleStackRightShiftable() {
+		return topThree + 1 < topTwo;
+	}
+
+	public static void main(String args[]) throws Exception {
+		ThreeStacksInArray tsia = new ThreeStacksInArray();
+		tsia.push(3, 1);
+		tsia.push(3, 1);
+		tsia.push(3, 1);
+		tsia.push(3, 1);
+		tsia.push(3, 1);
+		tsia.push(3, 1);
+	}
 }
