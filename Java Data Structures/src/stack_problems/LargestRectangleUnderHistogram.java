9c708ef7a563096343aa74b307b5cd67dd8ea807
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-01-17 11:07:17 +0530

    typo fixedin  largest rectangle under histogram

--- Java Data Structures/src/stack_problems/LargestRectangleUnderHistogram.java
+++ Java Data Structures/src/stack_problems/LargestRectangleUnderHistogram.java
@@ -1,6 +1,8 @@
+import java.util.Stack;
+
 public class LargestRectangleUnderHistogram {
 
-public static void main(String[] args[]) {
+public static void main(String[] args) {
 	System.out.println(getMaxArea(new int[]{6, 2, 5, 4, 5, 1, 6}));
 }
 
