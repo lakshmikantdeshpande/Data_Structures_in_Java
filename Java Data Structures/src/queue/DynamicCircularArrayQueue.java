08d0d3ef5deb730852195390c94bea6ec98bcdc1
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-01-20 02:04:23 +0530

    Implemented Queues
    Array Queue, Dynamic Array Queue & Linked List Queue

---
+++ Java Data Structures/src/queue/DynamicCircularArrayQueue.java
@@ -0,0 +1,102 @@
+// Circular dynamic array queue
+// o(n) space
+// o(1) time for enqueue, dequeue, size, isEmpty, isFull
+
+package queue;
+
+public class DynamicCircularArrayQueue {
+
+	private int size;
+	private int capacity;
+	private int mincapacity = 1 << 15;
+	private int front, rear;
+	private int[] queue;
+
+	public DynamicCircularArrayQueue(int capacity) {
+		this.capacity = capacity;
+		queue = new int[capacity];
+		front = rear = 0;
+		size = 0;
+	}
+
+	public DynamicCircularArrayQueue() {
+		this(16);
+	}
+
+	public synchronized void enQueue(int data) throws Exception {
+		if (isFull())
+			expand();
+		size++;
+		queue[rear] = data; // push the next incoming element
+							// at the available position
+		rear = (rear + 1) % capacity;
+	}
+
+	private void expand() {
+		int length = size();
+		int newQueue[] = new int[length << 1]; // twice the size
+		
+		for (int i = front; i <= rear; i++)
+			newQueue[i - front] = queue[i % capacity];
+		queue = newQueue;
+		front = 0;
+		rear = size - 1;
+		capacity *= 2;
+	}
+
+	public synchronized int deQueue() throws Exception {
+		if (isEmpty())
+			throw new Exception("Underflow");
+
+		size--;
+		int data = queue[front];
+		queue[front] = Integer.MIN_VALUE;
+		front = (front + 1) % capacity;
+		return data;
+	}
+	
+	private void shrink() {
+		int length = size;
+		if (length <= mincapacity || length << 2 >= length)
+			return;
+		
+		if (length < mincapacity)
+			length = mincapacity;
+		int newQueue[] = new int[length];
+		System.arraycopy(queue, 0, newQueue, 0, length+1);
+		queue = newQueue;
+	}
+
+	public synchronized boolean isEmpty() {
+		return size() == 0;
+	}
+
+	public synchronized int size() {
+		return this.size;
+	}
+
+	public synchronized boolean isFull() {
+		return size() == this.capacity;
+	}
+
+	@Override
+	public String toString() {
+		if (isEmpty())
+			return "[]";
+
+		StringBuilder strb = new StringBuilder("[ ");
+		int index = front;
+		for (int i = 0; i < size; i++) {
+			strb.append(queue[index] + " ");
+			index = (index + 1) % capacity;
+		}
+		return strb.append("]").toString();
+	}
+
+	public static void main(String[] args) throws Exception {
+		DynamicCircularArrayQueue dacq = new DynamicCircularArrayQueue();
+		dacq.enQueue(1);
+		System.out.println(dacq.toString());
+	}
+
+}
---
+++ Java Data Structures/src/queue/LinkedListQueue.java
@@ -0,0 +1,83 @@
+package queue;
+
+public class LinkedListQueue {
+
+	private int length;
+	private Node front, rear;
+
+	private static class Node {
+		int data;
+		Node next;
+
+		public Node(int data) {
+			this.data = data;
+			next = null;
+		}
+	}
+
+	public LinkedListQueue() {
+		length = 0;
+		front = rear = null;
+	}
+
+	public synchronized void enQueue(int data) throws Exception {
+		Node node = new Node(data);
+		if (isEmpty())
+			front = node;
+		else
+			rear.next = node;
+		rear = node;
+		length++;
+	}
+
+	public synchronized int deQueue() throws Exception {
+		if (isEmpty())
+			throw new Exception("Underflow");
+
+		int data = front.data;
+		front = front.next;
+		System.gc();
+		length--;
+
+		if (isEmpty())
+			rear = null;
+		return data;
+	}
+
+	public int peek() throws Exception {
+		if (isEmpty())
+			throw new Exception("Queue is empty!");
+		return front.data;
+	}
+
+	public boolean isEmpty() {
+		return length == 0;
+	}
+
+	public int size() {
+		return length;
+	}
+
+	@Override
+	public String toString() {
+		if (isEmpty())
+			return "[ ]";
+		StringBuilder strb = new StringBuilder("[ ");
+		for (Node temp = front; temp != null; temp = temp.next)
+			strb.append(temp.data + " ");
+		return strb.append("]").toString();
+	}
+
+	public static void main(String[] args) throws Exception {
+		LinkedListQueue llq = new LinkedListQueue();
+		llq.enQueue(5);
+		llq.enQueue(8);
+		llq.enQueue(89);
+		System.out.println(llq.toString());
+		llq.deQueue();
+		llq.deQueue();
+		llq.deQueue();
+		System.out.println(llq.toString());
+	}
+
+}
