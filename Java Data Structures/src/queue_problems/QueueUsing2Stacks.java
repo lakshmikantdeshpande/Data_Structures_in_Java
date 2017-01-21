0eaf70803c6f995ae4fd97b77a636d241c4160a1
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-01-21 13:21:11 +0530

    Stack using 2 queues & Queue using 2 stacks

---
+++ Java Data Structures/src/queue_problems/QueueUsing2Stacks.java
@@ -0,0 +1,45 @@
+// 2. Implement a queue using 2 stacks
+// dequeue n time worse 1 best
+// enqueue 1 time
+// n space
+
+package queue_problems;
+
+import java.util.Stack;
+
+public class QueueUsing2Stacks {
+	private Stack<Integer> stack1, stack2;
+
+	public QueueUsing2Stacks() {
+		stack1 = new Stack<Integer>();
+		stack2 = new Stack<Integer>();
+	}
+
+	public static void main(String[] args) throws Exception {
+		QueueUsing2Stacks quts = new QueueUsing2Stacks();
+		quts.enQueue(1);
+		quts.enQueue(2);
+		System.out.println(quts.deQueue());
+		quts.enQueue(6);
+		System.out.println(quts.deQueue());
+		System.out.println(quts.deQueue());
+	}
+
+	public synchronized void enQueue(int data) {
+		stack1.push(data);
+	}
+
+	// Micro Optimization
+	public synchronized int deQueue() throws Exception {
+		if (stack2.empty()) {
+			if (stack1.empty())
+				throw new Exception("Empty Queue");
+			// while (!S1.empty())
+			while (stack1.size() > 1)
+				stack2.push(stack1.pop());
+		}
+		// return S2.pop();
+		return stack1.pop();
+	}
+
+}
---
+++ Java Data Structures/src/queue_problems/StackUsing2Queues.java
@@ -0,0 +1,58 @@
+// 3. Implement a stack using 2 queues
+// 1 time push
+// n time pop worse
+// 1 time pop best
+// n space
+
+package queue_problems;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackUsing2Queues {
+	private Queue<Integer> queue1, queue2;
+
+	public StackUsing2Queues() {
+		queue1 = new LinkedList<>();
+		queue2 = new LinkedList<>();
+	}
+
+	public static void main(String[] args) throws Exception {
+		StackUsing2Queues su = new StackUsing2Queues();
+		su.push(1);
+		su.push(2);
+		su.push(3);
+		System.out.println(su.pop());
+		System.out.println(su.pop());
+		System.out.println(su.pop());
+	}
+
+	public synchronized void push(int data) {
+		if (queue1.isEmpty())
+			queue2.offer(data);
+		else
+			queue1.offer(data);
+	}
+
+	public synchronized int pop() throws Exception {
+		int i = 0, size;
+		if (queue2.isEmpty() && queue1.isEmpty())
+			throw new Exception("Stack is empty");
+		else if (queue2.isEmpty()) {
+			size = queue1.size();
+			while (i < size - 1) {
+				queue2.offer(queue1.poll());
+				i++;
+			}
+			return queue1.poll();
+		} else {
+			size = queue2.size();
+			while (i < size - 1) {
+				queue1.offer(queue2.poll());
+				i++;
+			}
+			return queue2.poll();
+		}
+	}
+
+}
