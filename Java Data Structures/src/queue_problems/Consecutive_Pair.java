54aed93c41b83e07567a6077d758888a7425422e
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-01-22 15:53:19 +0530

    Stack Consecutive Numbers
    Used queue to check if stack contains consecutive numbers or not

---
+++ Java Data Structures/src/queue_problems/Consecutive_Pair.java
@@ -0,0 +1,55 @@
+// 8. Given a stack of integers, how’d u check if a pair of numbers is consecutive or not. 
+// It can be in increasing or decreasing order. If stack has odd number of elements, 
+// top will be left out of a pair. 
+// n time (n+n+n+n)
+// n space
+
+package queue_problems;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Consecutive_Pair {
+
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(4);
+		stack.push(5);
+		stack.push(-2);
+		stack.push(-3);
+		stack.push(11);
+		stack.push(10);
+		stack.push(5);
+		stack.push(6);
+		stack.push(20);
+
+		System.out.println(consecutiveCheck(stack));
+	}
+
+	private static boolean consecutiveCheck(Stack<Integer> stack) {
+		Queue<Integer> queue = new LinkedList<Integer>();
+		boolean ordered = true;
+
+		while (!stack.empty())
+			queue.add(stack.pop());
+
+		while (!queue.isEmpty())
+			stack.push(queue.remove());
+
+		while (!stack.isEmpty()) {
+			int n = stack.pop();
+			queue.add(n);
+			if (!stack.isEmpty()) {
+				int m = stack.pop();
+				queue.add(m);
+				if (Math.abs(n - m) != 1)
+					ordered = false;
+			}
+		}
+		while (!queue.isEmpty())
+			stack.push(queue.remove());
+		return ordered;
+	}
+
+}
