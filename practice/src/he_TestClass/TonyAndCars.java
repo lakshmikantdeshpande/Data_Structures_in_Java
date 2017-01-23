7c9354c078b469ff05ca1051b8081d6a18235300
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-01-23 23:03:28 +0530

    He Code got accepted

--- practice/src/he_TestClass/TonyAndCars.java
+++ practice/src/he_TestClass/TonyAndCars.java
@@ -1,33 +1,26 @@
-package he_TestClass;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
-
+ 
 class TonyAndCars {
-	public static void main(String args[]) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-
-		Map<Integer, Long> map = new HashMap<Integer, Long>();
-		long nines = 9;
-		for (int i = 1; i < 20; i++) {
-			map.put(i, nines);
-			nines *= 10;
-		}
-
-		String str = "";
+    public static void main(String args[] ) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        Map<Integer, Long> map = new HashMap<Integer, Long>();
+        long nines = 9;
+        for (int i = 1; i < 12; i++) {
+            map.put(i, nines);
+            nines *= 10;
+        }
+ 
 		StringBuilder strb = new StringBuilder();
-
+ 
 		while (T-- > 0) {
-			str = br.readLine();
-			System.out.println("Input:" + str);
-			if (str.equals("")) {
-				continue;
-			}
-			long k = Long.parseLong(str);
+ 
+			long k = Long.parseLong(br.readLine());
 			long sum = 0;
 			long dsum = 0;
 			long t = 0;
@@ -47,9 +40,10 @@ class TonyAndCars {
 			strb.append(dsum + "\n");
 		}
 		System.out.println(strb.toString());
-		strb = null;
-		map = null;
-		br = null;
-		System.gc();
-	}
+ 
+        strb = null;
+        map = null;
+        br = null;
+        System.gc();
+    }
 }
