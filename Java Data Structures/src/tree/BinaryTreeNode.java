b3efa4eccf6cbe388003bb144a7a8e9103e81a2c
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-01-24 23:43:41 +0530

    Trees Started
    Simple Binary Tree Structure

---
+++ Java Data Structures/src/tree/BinaryTreeNode.java
@@ -0,0 +1,40 @@
+// basic structure of a binary tree
+
+package tree;
+
+public class BinaryTreeNode {
+	private int data;
+	private BinaryTreeNode left;
+	private BinaryTreeNode right;
+
+	public BinaryTreeNode(int data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public BinaryTreeNode getLeft() {
+		return left;
+	}
+
+	public void setLeft(BinaryTreeNode left) {
+		this.left = left;
+	}
+
+	public BinaryTreeNode getRight() {
+		return right;
+	}
+
+	public void setRight(BinaryTreeNode right) {
+		this.right = right;
+	}
+
+}
