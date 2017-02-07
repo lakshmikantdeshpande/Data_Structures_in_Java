9ce42d88580bdefe34dcf52b562f5992bd2373a3
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-02-07 13:25:35 +0530

    Print root to leaf paths complete

--- Java Data Structures/src/tree_problems/RootToLeafPaths.java
+++ Java Data Structures/src/tree_problems/RootToLeafPaths.java
@@ -15,19 +15,41 @@ public class RootToLeafPaths {
 		root.left.left = new TreeNode(4);
 		root.left.right = new TreeNode(5);
 
+		printPaths(root);
 	}
 
 	public static void printPaths(TreeNode root) {
 		int[] paths = new int[256];
-		printPaths(root, paths, 0); // root, paths array, length of path 
+		printPaths(root, paths, 0); // root, paths array, length of path
 	}
 
 	private static void printPaths(TreeNode root, int[] paths, int len) {
 		if (root == null)
 			return;
-		
+
 		// append this node to the paths array
 		paths[len++] = root.data;
-		
+
+		// Following code eases visualization
+		//
+		// System.out.print("{");
+		// for (int i = 0; i < len; i++) {
+		// System.out.print(paths[i] + " ");
+		// }
+		// System.out.println("}");
+
+		// if its a leaf, print the path
+		if (root.left == null && root.right == null)
+			print(paths, len);
+		else {
+			printPaths(root.left, paths, len);
+			printPaths(root.right, paths, len);
+		}
+	}
+
+	private static void print(int[] paths, int len) {
+		for (int i = 0; i < len; i++)
+			System.out.print(paths[i] + " ");
+		System.out.println();
 	}
 }
