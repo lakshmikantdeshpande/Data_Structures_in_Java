6d5fbbfa3c7ed0cd11370d98db90e6175fcfd929
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-02-16 13:47:29 +0530

    Print All Ancestors of a Node

---
+++ Java Data Structures/src/tree_problems/PrintAllAncestors.java
@@ -0,0 +1,34 @@
+// 32. Print all the ancestors of a node in a binary tree.
+// n time n space
+
+package tree_problems;
+
+import tree.TreeNode;
+
+public class PrintAllAncestors {
+
+	public static void main(String[] args) {
+		TreeNode root;
+		root = new TreeNode(1);
+		root.left = new TreeNode(2);
+		root.right = new TreeNode(3);
+		root.left.left = new TreeNode(4);
+		root.left.right = new TreeNode(5);
+
+		// printAllAncestors(root, new TreeNode(5));
+		printAllAncestors(root, root.left.right);
+	}
+
+	private static boolean printAllAncestors(TreeNode root, TreeNode node) {
+		if (root == null)
+			return false;
+		if (root.left == node || root.right == node
+				|| printAllAncestors(root.left, node)
+				|| printAllAncestors(root.right, node)) {
+			System.out.println(root.data);
+			return true;
+		}
+		return false;
+	}
+
+}
