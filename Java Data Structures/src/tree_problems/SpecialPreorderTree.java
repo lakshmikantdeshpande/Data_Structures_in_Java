860326debadb043020e3bd08774c96bdf21c7382
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-02-20 01:01:59 +0530

    Special Preorder Tree Complete

--- Java Data Structures/src/tree_problems/SpecialPreorderTree.java
+++ Java Data Structures/src/tree_problems/SpecialPreorderTree.java
@@ -1,13 +1,92 @@
+// 38. Given a preorder traversal of a tree represented by I and L where I is an internal node and
+// L is a leaf node, build a tree. Note that each node has either 0 or 2 nodes. 
+// n time n space                                         
+
+/*-
+ *                                 *********                                 
+ *                                 *       *                                 
+ *                                 *   I   *                                 
+ *                                 *       *                                 
+ *                                 *********                                
+ *                                **        **                              
+ *                              **            **                            
+ *                            **                **                          
+ *                          **                    **                        
+ *                        **                        **                      
+ *                   *********                       *********              
+ *                   *       *                       *       *              
+ *                   *   L   *                       *   I   *              
+ *                   *       *                       *       *              
+ *                   *********                       *********              
+ *                                                     *     *              
+ *                                                    *       **            
+ *                                                    *         *           
+ *                                                   *           *          
+ *                                                   *            **        
+ *                                                  *               *       
+ *                                                  *           **********  
+ *                                        **********            *        *  
+ *                                        *        *            *   L    *  
+ *                                        *   L    *            *        *  
+ *                                        *        *            **********  
+ *                                        **********                        
+ */
+
 package tree_problems;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+	char data;
+	TreeNode left, right;
+
+	public TreeNode(char c) {
+		this.data = c;
+	}
+}
 
 public class SpecialPreorderTree {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		levelOrderPrint(buildTree(new char[] { 'I', 'L', 'I', 'L', 'L' }, 0));
+	}
 
+	private static TreeNode buildTree(char[] array, int i) {
+		if (array == null || i == array.length)
+			return null;
+		TreeNode node = new TreeNode(array[i]);
+		if (array[i] == 'L')
+			return node;
+		node.left = buildTree(array, ++i);
+		node.right = buildTree(array, ++i);
+		return node;
+	}
+
+	private static void levelOrderPrint(TreeNode root) {
+		if (root == null)
+			return;
+
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.offer(root);
+		queue.offer(null);
+
+		while (!queue.isEmpty()) {
+			TreeNode temp = queue.poll();
+
+			if (temp != null) {
+				System.out.print(temp.data + " ");
+				if (temp.left != null)
+					queue.offer(temp.left);
+				if (temp.right != null)
+					queue.offer(temp.right);
+			} else {
+				System.out.println();
+				if (!queue.isEmpty())
+					queue.offer(null);
+			}
+		}
+		System.out.println("************************");
 	}
 
 }
