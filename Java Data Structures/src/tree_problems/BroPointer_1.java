11e546de6da23aaa70364bfcc2498f6d753868f0
Lakshmikant Anandkumar Deshpande <lakshmikantdeshpande@gmail.com>
2017-02-26 23:01:35 +0530

    Connect Bros in a tree
    Connect Bros at all level
    Recursive and non recursive solution

---
+++ Java Data Structures/src/tree_problems/BroPointer_1.java
@@ -0,0 +1,93 @@
+// 39. Fill next bro pointers if a tree is given with left, right, and nextBro pointer.
+// n time n space
+
+package tree_problems;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BroPointer_1 {
+
+	class BroNode {
+		int data;
+		BroNode left;
+		BroNode right;
+		BroNode nextBro;
+
+		public BroNode(int data) {
+			this.data = data;
+			left = right = nextBro = null;
+		}
+
+		public BroNode(int data, BroNode left, BroNode right, BroNode nextBro) {
+			this.data = data;
+			this.left = left;
+			this.right = right;
+			this.nextBro = nextBro;
+		}
+
+		public boolean isLeaf(BroNode broNode) {
+			return (broNode.left == null && broNode.right == null);
+		}
+	}
+
+	public static void main(String[] args) {
+		BroPointer_1 bp = new BroPointer_1();
+		BroNode root = bp.new BroNode(1);
+		root.left = bp.new BroNode(2);
+		root.right = bp.new BroNode(3);
+		root.left.left = bp.new BroNode(4);
+		root.left.right = bp.new BroNode(5);
+
+		fillBroPointers(root);
+		printTree(root.left);
+	}
+
+	public static void fillBroPointers(BroNode root) {
+		if (root == null)
+			return;
+
+		Queue<BroNode> queue = new LinkedList<BroNode>();
+		queue.offer(root);
+		queue.offer(null);
+
+		while (!queue.isEmpty()) {
+			BroNode temp = queue.poll();
+			if (temp != null) {
+				temp.nextBro = queue.peek();
+
+				if (temp.left != null)
+					queue.offer(temp.left);
+				if (temp.right != null)
+					queue.offer(temp.right);
+
+			} else {
+				if (!queue.isEmpty())
+					queue.offer(null);
+			}
+		}
+	}
+
+	public static void printTree(BroNode root) {
+		if (root == null)
+			return;
+
+		Queue<BroNode> queue = new LinkedList<BroNode>();
+		queue.offer(root);
+		queue.offer(null);
+
+		while (!queue.isEmpty()) {
+			BroNode temp = queue.poll();
+
+			if (temp != null) {
+				System.out.print(temp.data + " ");
+				if (temp.nextBro != null)
+					queue.offer(temp.nextBro);
+			} else {
+				if (!queue.isEmpty())
+					queue.offer(null);
+			}
+		}
+		System.out.println("************************");
+	}
+}
---
+++ Java Data Structures/src/tree_problems/BroPointer_2.java
@@ -0,0 +1,82 @@
+package tree_problems;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BroPointer_2 {
+
+	class BroNode {
+		int data;
+		BroNode left;
+		BroNode right;
+		BroNode nextBro;
+
+		public BroNode(int data) {
+			this.data = data;
+			left = right = nextBro = null;
+		}
+
+		public BroNode(int data, BroNode left, BroNode right, BroNode nextBro) {
+			this.data = data;
+			this.left = left;
+			this.right = right;
+			this.nextBro = nextBro;
+		}
+
+		public boolean isLeaf(BroNode broNode) {
+			return (broNode.left == null && broNode.right == null);
+		}
+	}
+
+	public static void main(String[] args) {
+		BroPointer_2 bp = new BroPointer_2();
+
+		BroNode root = bp.new BroNode(1);
+		root.left = bp.new BroNode(2);
+		root.right = bp.new BroNode(3);
+		root.left.left = bp.new BroNode(4);
+		root.left.right = bp.new BroNode(5);
+
+		fillBroPointers(root);
+		printTree(root.left);
+	}
+
+	public static void fillBroPointers(BroNode root) {
+		if (root == null)
+			return;
+		if (root.left != null)
+			root.left.nextBro = root.right;
+		if (root.right != null)
+			if (root.nextBro != null)
+				root.right.nextBro = root.nextBro.left;
+			else
+				root.right.nextBro = null;
+
+		fillBroPointers(root.left);
+		fillBroPointers(root.right);
+	}
+
+	public static void printTree(BroNode root) {
+		if (root == null)
+			return;
+
+		Queue<BroNode> queue = new LinkedList<BroNode>();
+		queue.offer(root);
+		queue.offer(null);
+
+		while (!queue.isEmpty()) {
+			BroNode temp = queue.poll();
+
+			if (temp != null) {
+				System.out.print(temp.data + " ");
+				if (temp.nextBro != null)
+					queue.offer(temp.nextBro);
+			} else {
+				if (!queue.isEmpty())
+					queue.offer(null);
+			}
+		}
+		System.out.println("************************");
+	}
+
+}
