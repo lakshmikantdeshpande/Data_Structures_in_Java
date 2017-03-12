79913948936e7ce2d2d490945e3040ea24122e0a
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-03-12 15:11:07 +0530

    Least common ancestor of a BST

--- Java Data Structures/src/tree/BST.java
+++ Java Data Structures/src/tree/BST.java
@@ -1,9 +1,11 @@
 package tree;
 
 public class BST {
-	int data;
-	BST left;
-	BST right;
+	// not an ideal structure
+	// we can make these private and use setters, getters
+	public int data;
+	public BST left;
+	public BST right;
 
 	public BST(int data) {
 		this.data = data;
---
+++ Java Data Structures/src/tree_problems/LeastCommonAncestorBST.java
@@ -0,0 +1,58 @@
+// 52. Find shortest path between 2 nodes in a BST.
+// n time 1 space (n space for recursion stack)
+
+package tree_problems;
+
+import tree.BST;
+
+public class LeastCommonAncestorBST {
+
+	public static void main(String[] args) {
+		BST root = insert(null, 50);
+
+		/*-
+		 *Let us create following BST      
+		 *       50                                   
+		 *     /     \                                 
+		 *    30      70                               
+		 *   /  \    /  \                              
+		 * 20   40  60   80 
+		 */
+
+		root = insert(root, 30);
+		root = insert(root, 20);
+		root = insert(root, 40);
+		root = insert(root, 70);
+		root = insert(root, 60);
+		root = insert(root, 80);
+
+		System.out.println(LCA(root, root.left.left, root.left.right).data);
+
+	}
+
+	private static BST LCA(BST root, BST a, BST b) {
+		if (root == null)
+			return null;
+		if (root == a || root == b)
+			return root;
+
+		BST left = LCA(root.left, a, b);
+		BST right = LCA(root.right, a, b);
+
+		if (left != null && right != null)
+			return root;
+		else
+			return left != null ? left : right;
+	}
+
+	// normal BST insertion
+	public static BST insert(BST root, int data) {
+		if (root == null)
+			return new BST(data);
+		if (data < root.data)
+			root.left = insert(root.left, data);
+		else if (data > root.data)
+			root.right = insert(root.right, data);
+		return root;
+	}
+}
