86fb065bd36e61ca0e8218b5529495452c3d331f
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-01-17 06:20:21 +0530

    Circular linked list

---
+++ practice/ds_revision/linkedlist/CircularLinkedList.java
@@ -0,0 +1,96 @@
+package linkedlist;
+
+public class CircularLinkedList {
+
+    private static Node tail;
+
+    private static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void insertBeginning(int data) {
+        if (tail == null) {
+            tail = new Node(data);
+            tail.next = tail;
+            return;
+        }
+
+        Node node = new Node(data);
+        node.next = tail.next;
+        tail.next = node;
+    }
+
+    public static void insertEnd(int data) {
+        insertBeginning(data);
+        tail = tail.next;
+    }
+
+    public static void insertMiddle(int data, int position) {
+        if (tail == null) {
+            System.out.println("Head is null, inserting at the beginning");
+            tail = new Node(data);
+            return;
+        } else if (position < 0) {
+            System.out.println("Invalid position, inserting at the beginning");
+            insertBeginning(data);
+            return;
+        }
+
+        if (position == 0) {
+            insertBeginning(data);
+        } else {
+            int counter = 1;
+            Node temp = tail.next;
+            while (temp != tail && counter < position) {
+                temp = temp.next;
+                counter++;
+            }
+
+            if (counter < position) {
+                System.out.println("Linked list is shorter than expected, inserting at the end of the linked list");
+                insertEnd(data);
+            } else {
+                Node t = new Node(data);
+                t.next = temp.next;
+                temp.next = t;
+            }
+        }
+    }
+
+    public static void display() {
+        StringBuilder builder = new StringBuilder();
+        Node temp = tail.next;
+        while (temp != tail) {
+            builder.append(temp.data).append("~");
+            temp = temp.next;
+        }
+        builder.append(temp.data);
+        System.out.println(builder.toString());
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i < 5) {
+                insertEnd(i);
+            } else {
+                insertBeginning(i);
+            }
+        }
+        display();
+
+        insertMiddle(11, -1);
+        display();
+        insertMiddle(111, 0);
+        display();
+        insertMiddle(1111, 5);
+        display();
+        insertMiddle(11111, 99);
+        display();
+    }
+
+}
