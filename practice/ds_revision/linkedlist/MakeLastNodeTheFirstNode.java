7d1f7f8309bbfe0ea398db536b4c4295b32df2e5
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-01-27 00:23:46 +0530

    Make last node a head

---
+++ practice/ds_revision/linkedlist/MakeLastNodeTheFirstNode.java
@@ -0,0 +1,31 @@
+package linkedlist;
+
+public class MakeLastNodeTheFirstNode {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            SLL.insertBeginning(i);
+        }
+        SLL.display();
+
+        SLL.head = makeLastNodeTheFirstNode(SLL.head);
+        SLL.display();
+    }
+
+    private static SLL.Node makeLastNodeTheFirstNode(SLL.Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        SLL.Node temp = head;
+        SLL.Node old = null;
+        while (temp.next != null) {
+            old = temp;
+            temp = temp.next;
+        }
+        old.next = null;
+        temp.next = head;
+        head = temp;
+        return head;
+    }
+
+}
