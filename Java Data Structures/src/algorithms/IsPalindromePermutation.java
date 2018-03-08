7fe6d03593354fb3768690237b274f1bfe027e84
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-03-08 07:35:00 +0530

    Faster method, no need of iteration again

--- Java Data Structures/src/algorithms/IsPalindromePermutation.java
+++ Java Data Structures/src/algorithms/IsPalindromePermutation.java
@@ -5,6 +5,8 @@ public class IsPalindromePermutation {
     public static void main(String[] args) {
         System.out.println(isPrimePermutation1("madam"));
         System.out.println(isPrimePermutation1("madamq"));
+        System.out.println(isPrimePermutation2("madam"));
+        System.out.println(isPrimePermutation2("madamq"));
     }
 
     private static boolean isPrimePermutation1(String string) {
@@ -16,13 +18,34 @@ public class IsPalindromePermutation {
 
         int[] frequencyMap = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
         for (char c : string.toCharArray()) {
-            frequencyMap[c  - 'a']++;
+            frequencyMap[c - 'a']++;
         }
 
         int oddCounter = 0;
         for (int i : frequencyMap) {
             if (i % 2 != 0) {
                 oddCounter++;
+            }
+        }
+
+        return oddCounter <= 1;
+    }
+
+    private static boolean isPrimePermutation2(String string) {
+        if (string == null) {
+            return false;
+        } else if (string.isEmpty()) {
+            return true;
+        }
+
+        int oddCounter = 0;
+        int[] frequencyMap = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+        for (char c : string.toCharArray()) {
+            frequencyMap[c - 'a']++;
+            if (frequencyMap[c - 'a'] % 2 != 0) {
+                oddCounter++;
+            } else {
+                oddCounter--;
             }
         }
 
