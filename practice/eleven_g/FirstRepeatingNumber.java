496ed5bd0b3f674c1ac3b8430d09257c8cbbe950
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-04-04 22:38:28 +0530

    Find first repeating number

--- practice/eleven_g/FirstRepeatingNumber.java
+++ practice/eleven_g/FirstRepeatingNumber.java
@@ -1,3 +1,10 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class FirstRepeatingNumber {
 
     // n * n time 1 space
@@ -20,13 +27,26 @@ public class FirstRepeatingNumber {
         throw new RuntimeException("All numbers appear unique");
     }
 
+    // N time N space
+    int findFirstRepeatingNumberFaster(int[] array) {
+        Set<Integer> set = new HashSet<>();
+        for (int number : array) {
+            if (!set.add(number)) {
+                return number;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         FirstRepeatingNumber frn = new FirstRepeatingNumber();
         int[] array = new int[]{1, 2, 3, 4, 5, 1, 3, 5, 7, 8};
         System.out.println(frn.findFirstRepeatingNumber(array));
+        System.out.println(frn.findFirstRepeatingNumberFaster(array));
 
         array = new int[]{1, 2, 3, 5, 6, 7, 8, 9, 10};
         System.out.println(frn.findFirstRepeatingNumber(array));
+        System.out.println(frn.findFirstRepeatingNumberFaster(array));
     }
 
 }
