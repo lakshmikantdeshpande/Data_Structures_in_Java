2c3ad404f74b9054fb7c4ca1ed8dd965bb219dcf
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-06-16 21:43:46 +0530

    Added XOR method

--- practice/preparation/amz/array/MissingNumber.java
+++ practice/preparation/amz/array/MissingNumber.java
@@ -5,14 +5,15 @@ import java.util.Arrays;
 public class MissingNumber {
 
     public static void main(String[] args) {
-        int[] array = new int[99];
+        int[] array = new int[100];
 
-        for (int i = 0; i < 99; i++) {
+        for (int i = 0; i < 100; i++) {
             array[i] = i >= 77 ? i + 1 : i;
         }
 
         System.out.println("Missing number is " + findMissingNumberBruteForce(array));
         System.out.println("Missing number is " + findMissingNumberOptimal(array));
+        System.out.println("Missing number is " + findMissingNumberXorOptimal(array));
     }
 
     // N time 1 space
@@ -37,6 +38,22 @@ public class MissingNumber {
         int sum = Arrays.stream(array).sum();
         int requiredSum = (array.length * (array.length + 1)) / 2;
         return requiredSum - sum;
+    }
+
+    // N time 1 space
+    private static int findMissingNumberXorOptimal(int[] array) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+        int actualXor = 0;
+        for (int i = 0; i < 100; i++) {
+            actualXor ^= array[i];
+        }
+        int requiredXor = 0;
+        for (int i = 0; i < 101; i++) {
+            requiredXor ^= i;
+        }
+        return requiredXor ^ actualXor;
     }
 
 }
