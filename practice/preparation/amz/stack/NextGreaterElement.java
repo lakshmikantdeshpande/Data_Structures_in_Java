82919a3cd8b7e7699cf61384f6f6889d0829e199
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-06-24 21:14:18 +0530

    Next Greater Element

---
+++ practice/preparation/amz/stack/NextGreaterElement.java
@@ -0,0 +1,32 @@
+package amz.stack;
+
+import java.util.Stack;
+
+public class NextGreaterElement {
+
+    public static void main(String[] args) {
+        int[] array = {11, 13, 21, 3, 8, 4, 3, 2, 9};
+        nextGreaterElements(array);
+    }
+
+    private static void nextGreaterElements(int[] array) {
+        if (array == null || array.length == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(array[0]);
+        final int n = array.length;
+
+        for (int i = 1; i < n; i++) {
+            if (!stack.isEmpty()) {
+                while (!stack.isEmpty() && stack.peek() < array[i]) {
+                    System.out.printf("%ds next greater element is %d%n", stack.pop(), array[i]);
+                }
+            }
+            stack.push(array[i]);
+        }
+
+    }
+
+}
